23ee35a4e1c17da7e7e7ed1f39bd465b52ee28cd
Skibidi50-lol <realhatroi@gmail.com>
2026-01-04 12:41:06 +0700

    Update StealTools.lua

--- scripts/sab/StealTools.lua
+++ scripts/sab/StealTools.lua
@@ -44,70 +44,6 @@ local Window = Luna:CreateWindow({
     }
 })
 
-
-local function performDesync()
-    local flags = {
-        {"GameNetPVHeaderRotationalVelocityZeroCutoffExponent", "-5000"},
-        {"LargeReplicatorWrite5", "true"},
-        {"LargeReplicatorEnabled9", "true"},
-        {"AngularVelociryLimit", "360"},
-        {"TimestepArbiterVelocityCriteriaThresholdTwoDt", "2147483646"},
-        {"S2PhysicsSenderRate", "15000"},
-        {"DisableDPIScale", "true"},
-        {"MaxDataPacketPerSend", "2147483647"},
-        {"ServerMaxBandwith", "52"},
-        {"PhysicsSenderMaxBandwidthBps", "20000"},
-        {"MaxTimestepMultiplierBuoyancy", "2147483647"},
-        {"SimOwnedNOUCountThresholdMillionth", "2147483647"},
-        {"MaxMissedWorldStepsRemembered", "-2147483648"},
-        {"CheckPVDifferencesForInterpolationMinVelThresholdStudsPerSecHundredth", "1"},
-        {"StreamJobNOUVolumeLengthCap", "2147483647"},
-        {"DebugSendDistInSteps", "-2147483648"},
-        {"MaxTimestepMultiplierAcceleration", "2147483647"},
-        {"LargeReplicatorRead5", "true"},
-        {"SimExplicitlyCappedTimestepMultiplier", "2147483646"},
-        {"GameNetDontSendRedundantNumTimes", "1"},
-        {"CheckPVLinearVelocityIntegrateVsDeltaPositionThresholdPercent", "1"},
-        {"CheckPVCachedRotVelThresholdPercent", "10"},
-        {"LargeReplicatorSerializeRead3", "true"},
-        {"ReplicationFocusNouExtentsSizeCutoffForPauseStuds", "2147483647"},
-        {"NextGenReplicatorEnabledWrite4", "true"},
-        {"CheckPVDifferencesForInterpolationMinRotVelThresholdRadsPerSecHundredth", "1"},
-        {"GameNetDontSendRedundantDeltaPositionMillionth", "1"},
-        {"InterpolationFrameVelocityThresholdMillionth", "5"},
-        {"StreamJobNOUVolumeCap", "2147483647"},
-        {"InterpolationFrameRotVelocityThresholdMillionth", "5"},
-        {"WorldStepMax", "30"},
-        {"TimestepArbiterHumanoidLinearVelThreshold", "1"},
-        {"InterpolationFramePositionThresholdMillionth", "5"},
-        {"TimestepArbiterHumanoidTurningVelThreshold", "1"},
-        {"MaxTimestepMultiplierContstraint", "2147483647"},
-        {"GameNetPVHeaderLinearVelocityZeroCutoffExponent", "-5000"},
-        {"CheckPVCachedVelThresholdPercent", "10"},
-        {"TimestepArbiterOmegaThou", "1073741823"},
-        {"MaxAcceptableUpdateDelay", "1"},
-        {"LargeReplicatorSerializeWrite4", "true"},
-    }
-    for _, data in ipairs(flags) do
-        pcall(function()
-            if setfflag then
-                setfflag(data[1], data[2])
-            end
-        end)
-    end
-    local char = player.Character
-    if not char then return end
-    local humanoid = char:FindFirstChildWhichIsA("Humanoid")
-    if humanoid then
-        humanoid:ChangeState(Enum.HumanoidStateType.Dead)
-    end
-    char:ClearAllChildren()
-    local fakeModel = Instance.new("Model", workspace)
-    player.Character = fakeModel
-    task.wait()
-    player.Character = char
-    fakeModel:Destroy()
-end
 --auto kick
 local autoKickEnabled = false
 local kickKeyword = "you stole"
@@ -296,7 +232,7 @@ end)
 --steal tab
 local StealTab = Window:CreateTab({
     Name = "Stealer",
-    Icon = "mobile_theft",
+    Icon = "view_in_ar",
     ImageSource = "Material",
     ShowTitle = true -- This will determine whether the big header text in the tab will show
 })
@@ -307,6 +243,69 @@ StealTab:CreateButton({
     Name = "Desync No Tool",
     Description = "May Causing Bugs",
     Callback = function()
+        function performDesync()
+            local flags = {
+                {"GameNetPVHeaderRotationalVelocityZeroCutoffExponent", "-5000"},
+                {"LargeReplicatorWrite5", "true"},
+                {"LargeReplicatorEnabled9", "true"},
+                {"AngularVelociryLimit", "360"},
+                {"TimestepArbiterVelocityCriteriaThresholdTwoDt", "2147483646"},
+                {"S2PhysicsSenderRate", "15000"},
+                {"DisableDPIScale", "true"},
+                {"MaxDataPacketPerSend", "2147483647"},
+                {"ServerMaxBandwith", "52"},
+                {"PhysicsSenderMaxBandwidthBps", "20000"},
+                {"MaxTimestepMultiplierBuoyancy", "2147483647"},
+                {"SimOwnedNOUCountThresholdMillionth", "2147483647"},
+                {"MaxMissedWorldStepsRemembered", "-2147483648"},
+                {"CheckPVDifferencesForInterpolationMinVelThresholdStudsPerSecHundredth", "1"},
+                {"StreamJobNOUVolumeLengthCap", "2147483647"},
+                {"DebugSendDistInSteps", "-2147483648"},
+                {"MaxTimestepMultiplierAcceleration", "2147483647"},
+                {"LargeReplicatorRead5", "true"},
+                {"SimExplicitlyCappedTimestepMultiplier", "2147483646"},
+                {"GameNetDontSendRedundantNumTimes", "1"},
+                {"CheckPVLinearVelocityIntegrateVsDeltaPositionThresholdPercent", "1"},
+                {"CheckPVCachedRotVelThresholdPercent", "10"},
+                {"LargeReplicatorSerializeRead3", "true"},
+                {"ReplicationFocusNouExtentsSizeCutoffForPauseStuds", "2147483647"},
+                {"NextGenReplicatorEnabledWrite4", "true"},
+                {"CheckPVDifferencesForInterpolationMinRotVelThresholdRadsPerSecHundredth", "1"},
+                {"GameNetDontSendRedundantDeltaPositionMillionth", "1"},
+                {"InterpolationFrameVelocityThresholdMillionth", "5"},
+                {"StreamJobNOUVolumeCap", "2147483647"},
+                {"InterpolationFrameRotVelocityThresholdMillionth", "5"},
+                {"WorldStepMax", "30"},
+                {"TimestepArbiterHumanoidLinearVelThreshold", "1"},
+                {"InterpolationFramePositionThresholdMillionth", "5"},
+                {"TimestepArbiterHumanoidTurningVelThreshold", "1"},
+                {"MaxTimestepMultiplierContstraint", "2147483647"},
+                {"GameNetPVHeaderLinearVelocityZeroCutoffExponent", "-5000"},
+                {"CheckPVCachedVelThresholdPercent", "10"},
+                {"TimestepArbiterOmegaThou", "1073741823"},
+                {"MaxAcceptableUpdateDelay", "1"},
+                {"LargeReplicatorSerializeWrite4", "true"},
+            }
+            for _, data in ipairs(flags) do
+                pcall(function()
+                    if setfflag then
+                        setfflag(data[1], data[2])
+                    end
+                end)
+            end
+            local char = player.Character
+            if not char then return end
+            local humanoid = char:FindFirstChildWhichIsA("Humanoid")
+            if humanoid then
+                humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+            end
+            char:ClearAllChildren()
+            local fakeModel = Instance.new("Model", workspace)
+            player.Character = fakeModel
+            task.wait()
+            player.Character = char
+            fakeModel:Destroy()
+        end
         performDesync()
     end
 })
