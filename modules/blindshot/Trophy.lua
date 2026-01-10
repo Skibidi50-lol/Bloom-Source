b3f40a93cbd09f7c5430af760b176b0a3492de7e
Skibidi50-lol <realhatroi@gmail.com>
2026-01-10 16:17:48 +0700

    Create Trophy.lua

---
+++ modules/blindshot/Trophy.lua
@@ -0,0 +1,17 @@
+getgenv().TrophyFarm = false
+    if getgenv().TrophyFarm then
+      spawn(function()
+        while _G.TrophyFarm do
+            local char = player.Character
+            local hrp = char and char:FindFirstChild("HumanoidRootPart")
+            local trophyPart = workspace:FindFirstChild("Trophy", true)
+                    
+            if trophyPart and hrp then
+                firetouchinterest(hrp, trophyPart, 0)
+                    task.wait()
+                firetouchinterest(hrp, trophyPart, 1)
+              end
+              task.wait(0.3)
+        end
+    end)
+end
