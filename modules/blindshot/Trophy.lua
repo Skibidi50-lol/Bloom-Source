a35ba9ba2edba5fc67f2c225acfbd7caf568b8f8
Skibidi50-lol <realhatroi@gmail.com>
2026-01-10 16:21:40 +0700

    Update Trophy.lua

--- modules/blindshot/Trophy.lua
+++ modules/blindshot/Trophy.lua
@@ -1,17 +1,15 @@
 getgenv().TrophyFarm = false
-    if getgenv().TrophyFarm then
-      spawn(function()
-        while _G.TrophyFarm do
-            local char = player.Character
-            local hrp = char and char:FindFirstChild("HumanoidRootPart")
-            local trophyPart = workspace:FindFirstChild("Trophy", true)
+spawn(function()
+    while getgenv().TrophyFarm do
+        local char = player.Character
+        local hrp = char and char:FindFirstChild("HumanoidRootPart")
+        local trophyPart = workspace:FindFirstChild("Trophy", true)
                     
-            if trophyPart and hrp then
-                firetouchinterest(hrp, trophyPart, 0)
-                    task.wait()
-                firetouchinterest(hrp, trophyPart, 1)
-              end
-              task.wait(0.3)
+        if trophyPart and hrp then
+            firetouchinterest(hrp, trophyPart, 0)
+                task.wait()
+            firetouchinterest(hrp, trophyPart, 1)
         end
-    end)
-end
+        task.wait(0.3)
+    end
+end)
