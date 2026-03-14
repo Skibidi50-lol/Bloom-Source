e98342929bd02ffbe5b2231def357fdf801616ca
Skibidi50-lol <realhatroi@gmail.com>
2026-03-14 16:57:06 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,16 @@
-SON
+local Scripts = {
+    -- Garden Horizons
+    [130594398886540] = "https://raw.githubusercontent.com/Skibidi50-lol/Bloom-Source/refs/heads/main/scripts/GardenHorizons.lua",
+
+    -- BasketBall legends
+    [4931927012] = "github.com/Skibidi50-lol/Bloom-Source/blob/main/scripts/BasketballLegends.lua",
+    -- Arsenal
+    [4931927012] = "github.com/Skibidi50-lol/Bloom-Source/blob/main/scripts/BasketballLegends.lua",
+}
+
+local ScriptURL = Scripts[game.PlaceId]
+if ScriptURL then
+    loadstring(game:HttpGet(ScriptURL))()
+else
+    game.Players.LocalPlayer:Kick("BloomWare | This game is not supported. Copied to clipboard link with supported games")
+end
